ca807cdc721c53dacb161b483426a988eecfc8a5
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-05 01:37:52 -0500

    Add test coverage for parameters and objective function helpers

--- generateTestParameters.m
+++ generateTestParameters.m
@@ -1,9 +1,11 @@
 function [parameters] = generateTestParameters()
 %GENERATETESTPARAMETERS generate a set of parameters for testing
-    SEED = 1234;
+    %% setup
+    SEED = 123;
     rng(SEED);  % set the random seed for reproducibility
 
     p = struct();
+    utils = support();
     
     %% given data
     p.N = 5;            %   number of stations
@@ -11,7 +13,7 @@ function [parameters] = generateTestParameters()
     p.C = 100;          %   daily budget for incentives
     p.lambda = 2.5;     %   cost to rebalance a bike manually
     p.alphaO = 0.75;    %   cooperativeness parameter for switching origins
-    p.alphaD = 0.85;    %   cooperativeness parameter for switching destinations
+    p.alphaD = 0.5;    %   cooperativeness parameter for switching destinations
     p.b = randi([5, 10], p.N, 1);   % station capacity
 
     cO = rand(p.N) * 5;
@@ -28,14 +30,14 @@ function [parameters] = generateTestParameters()
     
     fout = squeeze(sum(reshape(p.dO, p.N, p.N, p.T), 2));
     p.fout = fout(:);   % number of bikes leaving station i at time t
-    fin = squeeze(sum(reshape(p.dD, p.N, p.N, p.T), 2));
+    fin = squeeze(sum(reshape(p.dD, p.N, p.N, p.T), 1));
     p.fin = fin(:);     % number of bikes arriving to station i at time t
     
     %TODO: pull these out to their own functions
-    p.foutstar = @(dstarO)(computeIncentivizedFlow(p.alphaO, p.dOinc, dstarO)); % incentivized flow out
-    p.finstar = @(dstarD)(computeIncentivizedFlowin(p.alphaD, p.dDinc, dstarD)); % incentivized flow in
+    p.foutstar = @(dstarO)(utils.computeIncentivizedFlow(p, p.alphaO, p.dOinc, dstarO)); % incentivized flow out
+    p.finstar = @(dstarD)(utils.computeIncentivizedFlow(p, p.alphaD, p.dDinc, dstarD)); % incentivized flow in
     
-    p.fhat = @(dstarO, dstarD)((p.fin + p.finstar(dstarO)) - (p.fout + p.foutstar(dstarD))); % net flow
+    p.fhat = @(dstarO, dstarD)((p.fin + p.finstar(dstarD)) - (p.fout + p.foutstar(dstarO))); % net flow
 
     parameters = p;
 end
@@ -51,18 +53,9 @@ function [demand] = populateDemandMatrix(p, n)
         i = randi([1, p.N]);
         j = randi([1, p.N]);
         t = randi([1, p.T]);
-        demand(i, j, t) = demand(i, j, t) + 1;
+        if i ~= j % no demand from station to itself
+            demand(i, j, t) = demand(i, j, t) + 1;
+        end
     end
     demand = demand(:);
-end
-
-function [f] = computeIncentivizedFlow(alpha, dinc, dstar)
-    f = (alpha * dstar + p.dOinc) - ...
-        (alpha * swapStationIndices(dstar) + swapStationIndices(dinc));
-    f = squeeze(sum(f, 2));
-    f = f(:);
-end
-
-function [b] = swapStationIndices(d)
-    b = permute(d,[2, 1, 3]);
 end
--- srcTest.m
+++ srcTest.m
@@ -1,7 +1,123 @@
-%% Test generated parameters
+classdef srcTest < matlab.unittest.TestCase
+    methods (Test)
+        function testGenerateParameters(testCase)
+            p = generateTestParameters();
+            testCase.assertEqual(sum(p.dO), sum(p.s));
+            testCase.assertEqual(sum(p.dD), sum(p.s));
+            testCase.assertEqual(sum(p.dOinc), 3);
+            testCase.assertEqual(sum(p.dDinc), 4);
+        end
 
-p = generateTestParameters();
-assert(sum(p.dO) == sum(p.s));
-assert(sum(p.dD) == sum(p.s));
-assert(sum(p.dOinc) == 3);
-assert(sum(p.dDinc) == 4);
+        function testToStationTimeIndex(testCase)
+            utils = support();
+            p = generateTestParameters();
+
+            d1 = zeros(p.N*p.T, 1);
+            testCase.assertEqual(size(utils.toStationTimeIndex(p, d1)), [p.N, p.T]);
+            d2 = zeros(p.N*p.N*p.T, 1);
+            testCase.assertEqual(size(utils.toStationTimeIndex(p, d2)), [p.N,  p.N, p.T]);
+            d3 = zeros(p.N, p.N);
+            testCase.assertError(@()(utils.toStationTimeIndex(p, d3)),  "toStationTimeIndex:badDimension");
+            d4 = zeros(p.N, 1);
+            testCase.assertError(@()(utils.toStationTimeIndex(p, d4)), "toStationTimeIndex:badNumel");
+        end
+
+        function testfin(testCase)
+            p = generateTestParameters();
+            u = support();
+            dD = u.toStationTimeIndex(p, p.dD);
+            expected = zeros(p.N, p.T);
+            for i = 1:p.N
+                for t = 1:p.T
+                    for j = 1:p.N
+                        expected(i, t) = expected(i, t) + dD(j, i, t);
+                    end
+                end
+            end
+            actual = p.fin;
+            testCase.assertEqual(actual, expected(:));
+        end
+
+        function testfout(testCase)
+            p = generateTestParameters();
+            u = support();
+            dO = u.toStationTimeIndex(p, p.dO);
+            expected = zeros(p.N, p.T);
+            for i = 1:p.N
+                for t = 1:p.T
+                    for j = 1:p.N
+                        expected(i, t) = expected(i, t) + dO(i, j, t);
+                    end
+                end
+            end
+            actual = p.fout;
+            testCase.assertEqual(actual, expected(:));
+        end
+
+        function testfoutstar(testCase)
+            p = generateTestParameters();
+            u = support();
+            dstarO = randi(2, p.N, p.N, p.T);
+            dOinc = u.toStationTimeIndex(p, p.dOinc);
+            actual = p.foutstar(dstarO(:));
+            expected = zeros(p.N, p.T);
+            for i = 1:p.N
+                for t = 1:p.T
+                    for j = 1:p.N
+                        expected(i, t) = expected(i, t) + ...
+                                         (p.alphaO * dstarO(j, i, t) + dOinc(j, i, t)) - ...
+                                         (p.alphaO * dstarO(i, j, t) + dOinc(i, j, t));
+                    end
+                end
+            end
+            testCase.assertEqual(actual, expected(:));
+        end
+
+        function testfinstar(testCase)
+            p = generateTestParameters();
+            u = support();
+            dstarD = randi(2, p.N, p.N, p.T);
+            dDinc = u.toStationTimeIndex(p, p.dDinc);
+            actual = p.finstar(dstarD(:));
+            expected = zeros(p.N, p.T);
+            for i = 1:p.N
+                for t = 1:p.T
+                    for j = 1:p.N
+                        expected(i, t) = expected(i, t) + ...
+                                         (p.alphaD * dstarD(j, i, t) + dDinc(j, i, t)) - ...
+                                         (p.alphaD * dstarD(i, j, t) + dDinc(i, j, t));
+                    end
+                end
+            end
+            testCase.assertEqual(actual, expected(:));
+        end
+
+        function testfhat(testCase)
+            p = generateTestParameters();
+            u = support();
+            dO = u.toStationTimeIndex(p, p.dO);
+            dD = u.toStationTimeIndex(p, p.dD);
+            dstarO = randi(2, p.N, p.N, p.T);
+            dstarD = randi(2, p.N, p.N, p.T);
+            dOinc = u.toStationTimeIndex(p, p.dOinc);
+            dDinc = u.toStationTimeIndex(p, p.dDinc);
+            actual = p.fhat(dstarO(:), dstarD(:));
+            expected = zeros(p.N, p.T);
+            for i = 1:p.N
+                for t = 1:p.T
+                    for j = 1:p.N
+                        expected(i, t) = expected(i, t) + ...
+                                         (dD(j, i, t) + ...
+                                          (p.alphaD * dstarD(j, i, t) + dDinc(j, i, t)) - ...
+                                          (p.alphaD * dstarD(i, j, t) + dDinc(i, j, t))) - ...
+                                         (dO(i, j, t) + ...
+                                          (p.alphaO * dstarO(j, i, t) + dOinc(j, i, t)) - ...
+                                          (p.alphaO * dstarO(i, j, t) + dOinc(i, j, t)));
+                    end
+                end
+            end
+            testCase.assertEqual(actual, expected(:));
+        end
+
+    end
+end
