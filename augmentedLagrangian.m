453124f0a7a4fcb9bd8dd581658866bf6b8dafa6
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-05 04:02:31 -0500

    Add augmented lagrangian calculation with tests

--- augmentedLagrangian.m
+++ augmentedLagrangian.m
@@ -1,9 +1,11 @@
-function [differentiable, nondifferentiable] = augmentedLagrangian(parameters, dstar, u)
-%AUGMENTEDLAGRANGIAN Returns the differentiable and non-differentiable components of the augmented lagrangian 
+function [value] = augmentedLagrangian(parameters, c, dstar, u)
+%AUGMENTEDLAGRANGIAN Returns the value of the augmented lagrangian 
 %   Inputs
 %       parameters  (struct)    parameter struct
 %       dstar       (nx1)       optimization variables
 %       u           (mx1)       lagrange multipliers
-
-
+    p = parameters;
+    f = objective(p, dstar);
+    g = constraints(p, dstar);
+    value = f + (1/(2*c)) * sum( max(0, u + c * g).^2 - u.^2 );
 end
