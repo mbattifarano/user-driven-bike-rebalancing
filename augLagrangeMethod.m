42645647dbd4dac80a2a432fa1bbd968125fae6f
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-06 01:59:21 -0500

    First pass of the full method with basic test

---
+++ augLagrangeMethod.m
@@ -0,0 +1,12 @@
+function [x, u] = augLagrangeMethod(parameters, opts, u_init, x_init)
+%AUGLAGRANGEMETHOD
+    x = x_init;
+    u = u_init;
+    c = opts.c0;
+    for i = 1:opts.nIter
+        x = subgradientDescent(parameters, opts.innerIter, c, u, x);
+        c = opts.beta * c;
+        u = max(0, u + c * constraints(parameters, x));
+    end
+end
+
---
+++ subgradientDescent.m
@@ -0,0 +1,12 @@
+function [x] = subgradientDescent(parameters, nIter, ck, u, dstar_init)
+%SUBGRADIENTDESCENT
+    x = dstar_init;
+    for i = 1:nIter
+        x = x + stepSize(i) * subgradAL(parameters, ck, u, x);
+    end
+end
+
+function [t] = stepSize(k)
+    t = 1/(k^2);
+end
+
