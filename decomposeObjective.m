a7084eaa8f52e471c59e33e839f40f9d786b3ff8
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-06 02:17:07 -0500

    Remove unused functions

--- decomposeObjective.m
+++
@@ -1,8 +0,0 @@
-function [differentiable, nondifferentiable] = decomposeObjective(parameters, dstar)
-%DECOMPOSEOBJECTIVE Decompose objective function into its differentiable
-%and non differentiable components
-%   INPUTS
-%       parameters  struct  parameter and data struct
-%       dstar       nx1     objective variable
-end
-
