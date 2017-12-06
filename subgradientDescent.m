1cd3ca3ab6960102ca8bb2fd7e22fbd6048bddea
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-06 18:37:13 -0500

    add TODOs

--- subgradientDescent.m
+++ subgradientDescent.m
@@ -2,6 +2,7 @@ function [x] = subgradientDescent(parameters, nIter, ck, u, dstar_init)
 %SUBGRADIENTDESCENT
     x = dstar_init;
     for i = 1:nIter
+        % TODO: save state to history struct
         x = x + stepSize(i) * subgradAL(parameters, ck, u, x);
     end
 end
