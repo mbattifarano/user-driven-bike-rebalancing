9fc2d0147c511fa92f787e0339201c4b95271fca
danlm <danmartin2u@gmail.com>
2017-12-07 18:10:40 -0500

    Script to generate incentive matrices

---
+++ stationdata.m
@@ -0,0 +1,35 @@
+function station_struct = stationdata(system, origin_comp, dest_comp, incentive_method)
+
+
+
+% Import and sort data from files
+if strcmp(system,'Denver')
+    october2016kioskinfo = unique(importdenverkiosk('october2016_kioskinfo.csv', 2, 90));
+    dist = createdistancematrix(october2016kioskinfo.Latitude,october2016kioskinfo.Longitude);
+    station_struct.capacity = rmfield(october2016kioskinfo,{'Latitude','Longitude'});
+elseif strcmp(system,'Minneapolis')
+    NiceRide2016StationLocations = unique(importminnkiosk('Nice_Ride_2016_Station_Locations.csv', 2, 203));
+    dist = createdistancematrix(NiceRide2016StationLocations.Latitude,NiceRide2016StationLocations.Longitude);
+    station_struct.capacity = rmfield(NiceRide2016StationLocations,{'Latitude','Longitude'});
+elseif strcmp(system,'London')
+    londonkiosks = importlondonkiosk('bikepoint.json');
+    dist = createdistancematrix(londonkiosks.lat,londonkiosks.lon);
+    station_struct.capacity = rmfield(londonkiosks.nbdocks,{'lat','lon'});
+end
+
+% construct incentive matrix
+if strcmp(incentive_method,'exponential_utility')
+station_struct.origin_incentive = (1-exp(-origin_comp*dist))/origin_comp;
+station_struct.destination_incentive = (1-exp(-dest_comp*dist))/dest_comp;
+elseif strcmp(incentive_method,'isoelastic_utility')
+station_struct.origin_incentive = ((dist^origin_comp)-1)/origin_comp;
+station_struct.destination_incentive = ((dist^dest_comp)-1)/dest_comp;
+elseif strcmp(incentive_method,'linear_utility')
+station_struct.origin_incentive = dist*origin_comp;
+station_struct.destination_incentive = dist*dest_comp;
+elseif strcmp(incentive_method,'translog_utility') % formula to be confirmed
+station_struct.origin_incentive = ln(dist)*origin_comp;
+station_struct.destination_incentive = ln(dist)*dest_comp;
+end
+
+end
