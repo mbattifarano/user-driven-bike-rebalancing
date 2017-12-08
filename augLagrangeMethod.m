3b5d8cc983409fc93562d7c3f4696f5975a6aca2
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-08 00:37:46 -0500

    keep track of history

--- augLagrangeMethod.m
+++ augLagrangeMethod.m
@@ -1,12 +1,14 @@
-function [x, u] = augLagrangeMethod(parameters, opts, u_init, x_init)
+function [x, u, history] = augLagrangeMethod(parameters, opts, u_init, x_init)
 %AUGLAGRANGEMETHOD
     x = x_init;
     u = u_init;
     c = opts.c0;
+    history = [];
     for i = 1:opts.nIter
         % TODO: Save state to history struct
-        x = subgradientDescent(parameters, opts.innerIter, c, u, x);
+        [x, h] = subgradientDescent(parameters, opts.innerIter, c, u, x);
         c = opts.beta * c;
-        u = max(0, u + c * constraints(parameters, x));
+        u = max(0,  u + c * constraints(parameters, x));
+        history = [history h];
     end
 end
