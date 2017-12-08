27b24db2f120c894aa7e246e89c3c90704e1b2f9
Matt Battifarano <matthew.battifarano@gmail.com>
2017-12-08 00:37:46 -0500

    keep track of history

--- subgradientDescent.m
+++ subgradientDescent.m
@@ -1,10 +1,14 @@
-function [x] = subgradientDescent(parameters, nIter, ck, u, dstar_init)
+function [x, history] = subgradientDescent(parameters, nIter, ck, u, dstar_init)
 %SUBGRADIENTDESCENT
+    objValues = zeros(nIter, 1);
     x = dstar_init;
     for i = 1:nIter
         % TODO: save state to history struct
         x = x + stepSize(i) * subgradAL(parameters, ck, u, x);
+        objValues(i) = objective(parameters, x);
     end
+    history = struct();
+    history.objectiveValues = objValues;
 end
 
 function [t] = stepSize(k)
